06db54e1966f34f32c1f43f948f25e3cdf509625
muxudiinkeey <57723477+muxudiinkeey@users.noreply.github.com>
2022-04-03 15:10:53 +0200

    Initial commit

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+import { SharedModule } from '../shared/shared.module';
+
+const routes: Routes=[
+  {path:'products',component: ProductListComponent},
+  {path:'products/:id',component: ProductDetailComponent},
+  {path:'products/:id/edit',component: ProductEditComponent}
+]
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductEditComponent
+  ]
+})
+export class ProductModule { }
